c006e8c9f689437682b987323a07700cb5ead6be
t84787704-hue <t84787704@gmail.com>
2026-09-08 16:58:29 +0300

    feat(profile): add cloud_firestore dependency

--- lib/screens/gamer_profile_screen.dart
+++ lib/screens/gamer_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 import 'package:share_plus/share_plus.dart';
