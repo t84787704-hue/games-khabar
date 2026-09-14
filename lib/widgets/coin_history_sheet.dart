dd7bd0ca69a31f67913976663be9a09bb2428940
t84787704-hue <t84787704@gmail.com>
2026-09-14 18:59:57 +0300

    feat: add cloud_firestore import to coin history

--- lib/widgets/coin_history_sheet.dart
+++ lib/widgets/coin_history_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 import '../constants/gamer_theme.dart';
 import '../models/coin_transaction_model.dart';
